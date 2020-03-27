1fc3887c136d1ba1d21a52db508c6875e8e14794
Dzmitry Malyshau <kvarkus@gmail.com>
2020-03-27 00:22:02 -0400

    Enable more warnings for the descriptor crate

--- gfx-descriptor/src/lib.rs
+++ gfx-descriptor/src/lib.rs
@@ -1,3 +1,15 @@
+//! GPU descriptor allocator
+//!
+
+#![warn(
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
+
 mod allocator;
 mod counts;
 

9607036c2e538de0ee120f1d26fce7ba45f7be4b
Dzmitry Malyshau <kvarkus@gmail.com>
2020-10-09 21:34:29 -0400

    [memory] compute max chunk size as a fraction of heap memory

--- fuzz/fuzz_targets/general.rs
+++ fuzz/fuzz_targets/general.rs
@@ -2,12 +2,13 @@
 
 use arbitrary::{Arbitrary, Error, Unstructured};
 use gfx_fuzz::*;
-use gfx_memory::{GeneralAllocator, GeneralConfig};
+use gfx_memory::{GeneralAllocator, GeneralConfig, Size};
 use hal::{memory::Properties, MemoryTypeId};
 
 #[derive(Debug)]
 struct FuzzingInput {
     config: GeneralConfig,
+    total_memory: Size,
     allocations: Vec<Allocation>,
 }
 
@@ -15,16 +16,13 @@ impl Arbitrary for FuzzingInput {
     fn arbitrary(u: &mut Unstructured) -> Result<Self, Error> {
         let config = GeneralConfig {
             block_size_granularity: *u.choose(POWERS_OF_TWO)?,
-            max_chunk_size: *u.choose(POWERS_OF_TWO)?,
+            max_chunk_size_as_heap_total_fraction: *u.choose(POWERS_OF_TWO)? as usize,
             min_device_allocation: *u.choose(POWERS_OF_TWO)?,
         };
-        // Need to make sure the values make some sense
-        if config.min_device_allocation > config.max_chunk_size {
-            return Err(Error::IncorrectFormat);
-        }
         let allocations = u.arbitrary()?;
         let input = Self {
             config,
+            total_memory: *u.choose(POWERS_OF_TWO)? << 20,
             allocations,
         };
         Ok(input)
@@ -32,6 +30,12 @@ impl Arbitrary for FuzzingInput {
 }
 
 libfuzzer_sys::fuzz_target!(|input: FuzzingInput| {
-    let allocator = GeneralAllocator::new(MemoryTypeId(0), Properties::empty(), input.config, 1);
+    let allocator = GeneralAllocator::new(
+        MemoryTypeId(0),
+        Properties::empty(),
+        input.config,
+        1,
+        input.total_memory,
+    );
     perform_allocations(allocator, input.allocations);
 });
